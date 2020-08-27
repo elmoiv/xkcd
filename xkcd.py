290e2cd22f73788607021c267e239ec342989c15
Khaled ElMorshedy <elmoiv@yahoo.com>
2020-08-27 17:41:24 +0200

    Add files via upload

---
+++ xkcd.py
@@ -0,0 +1,67 @@
+from urllib.request import urlopen, urlretrieve
+import os
+
+os.makedirs('XKCD', exist_ok=True)
+os.chdir('XKCD')
+
+# Getting main info to get total images count
+info = eval(urlopen(f'https://xkcd.com/info.0.json').read())
+
+BIG_N = int(info['num'])
+IMAGES = os.listdir()
+CODE = True
+N = 0
+
+print(f'xkcd has {BIG_N} images')
+
+while N < BIG_N:
+    N += 1
+
+    # xkcd real-life meme, lol XD
+    if N == 404:
+        continue
+
+    # Check if this one exists
+    if any(
+        map(
+            lambda i: i in IMAGES,
+            map(
+                ''.join,
+                zip(
+                    [f'{N}.'] * 4,
+                    ['png', 'jpg', 'gif', 'jpeg']
+                    )
+                )
+            )
+        ):
+        continue
+
+    try:
+        # Get image url from API instead of parsing [Faster]
+        json = eval(urlopen(f'https://xkcd.com/{N}/info.0.json').read())
+
+        img_url = json['img']
+
+        # Check if url has no comic (Magic comic)
+        if img_url.endswith('/comics/'):
+            print(f'No comic at: {N}')
+            continue
+
+        # Decide extension and name
+        ext = img_url.split('.')[-1]
+        img_name = f'{N}.{ext}'
+
+        # Download image
+        urlretrieve(
+            img_url,
+            img_name
+            )
+
+        print(f'Saved: {img_name}')
+
+    # Catch error and inform user
+    except BaseException as e:
+        print(f'Error at {N}: {e}')
+        continue
+
+print('Finished :)')
